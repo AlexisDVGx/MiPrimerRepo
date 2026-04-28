1073563e4f9f2b86c10309e907403b4641bf62e8
AlexisDVGx <alexisvelazquez967@gmail.com>
2026-04-28 13:38:40 -0600

    Commit de cambios

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from funcion_n import suma, resta, multiplicacion
-
+import numpy as np
+import matplotlib.pyplot as mat
 a = suma(1,2)
 print(a)
 b = resta(1,2)
 print(b)
 c = multiplicacion(1,2)
 print(c)
+
+x = np.linspace(0,10)
+grafica = x + 2
+
+mat.figure()
+mat.plot(x,grafica)
+mat.legend()
+mat.grid()
+mat.show()
+
+mat.savefig("Grafica.eps")
